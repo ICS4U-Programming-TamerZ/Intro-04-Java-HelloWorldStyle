d63f10821f3140b68d717b78d661c8c0892e9152
Tamer Zreg <tamer.zreg@stu.ocsb.ca>
2024-02-10 01:51:46 +0000

    Added HelloWorldStyle.java

---
+++ HelloWorldStyle.java
@@ -0,0 +1,16 @@
+/**
+ * Prints Hello world with Style.
+ *
+ * @author Tamer
+ * @version 1.0
+ * @since 2024/02/08
+ */
+
+// Class
+public class HelloWorldStyle {
+  // Main Entry point method
+  public static void main(String[] args) {
+    // Output to Console saying "Hello, World!".
+    System.out.println("Hello World, with Style!");
+  }
+}
