326532022c639e81ae454a1818a4b64af8008cd2
Tamer Zreg <tamer.zreg@stu.ocsb.ca>
2024-02-14 22:51:09 +0000

    Fixed Linter

--- com/example/HelloWorldStyle.java
+++ com/example/HelloWorldStyle.java
@@ -11,20 +11,18 @@ package com.example;
 // Class
 public class HelloWorldStyle {
 
-    /**
-     * This is a private constructor used to prevent instantiation.
-     */
-    private HelloWorldStyle() {
-        throw new UnsupportedOperationException("This class cannot be instantiated.");
-    }
+  /** This is a private constructor used to prevent instantiation. */
+  private HelloWorldStyle() {
+    throw new UnsupportedOperationException("This class cannot be instantiated.");
+  }
 
-    /**
-     * This is the main method.
-     *
-     * @param args Unused
-     */
-    public static void main(final String[] args) {
-        System.out.println("Hello Van, with Style!");
-        System.out.println("How are you!");
-    }
+  /**
+   * This is the main method.
+   *
+   * @param args Unused
+   */
+  public static void main(final String[] args) {
+    System.out.println("Hello Van, with Style!");
+    System.out.println("How are you!");
+  }
 }
